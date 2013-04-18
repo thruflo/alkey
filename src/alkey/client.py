8f0f3bcca40ff61148125e5dd35fb9923b5eb6f2
James Arthur <thruflo@gmail.com>
2013-04-18 18:52:40 +0100

    client: use a blocking connection pool if available.

--- src/alkey/client.py
+++ src/alkey/client.py
@@ -167,7 +167,10 @@ def get_redis_client(request=None, env=None, get_config=None, get_registry=None,
     if redis_cls is None:
         redis_cls = redis.StrictRedis
     if pool_cls is None:
-        pool_cls = redis.ConnectionPool
+        try: # https://github.com/andymccurdy/redis-py/pull/340
+            pool_cls = redis.BlockingConnectionPool
+        except ImportError:
+            pool_cls = redis.ConnectionPool
     
     # Get the target ``registry``.
     has_registry = request and hasattr(request, 'registry')
