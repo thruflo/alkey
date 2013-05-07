2b46999559128ea769b529bd9191e9db132c3383
James Arthur <thruflo@gmail.com>
2013-05-07 12:54:55 +0100

    debug: absolute paths in SOURCES.txt

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(file_name):
 
 setup(
     name = 'alkey',
-    version = '0.3.1',
+    version = '0.3.1.1',
     description = 'Redis backed tool for generating cache keys.',
     author = 'James Arthur',
     author_email = 'username: thruflo, domain: gmail.com',
@@ -42,6 +42,6 @@ setup(
         'zope.component',
         'zope.interface',
         'sqlalchemy',
-        'redis'
+        'redis',
     ]
 )
